1f6d6023190bfbb0194c7e00162e0ba18ede5637
Nicholas Otto <43212390+nso332@users.noreply.github.com>
2018-12-09 16:16:44 -0600

    Finished Tetris
    Finished Tetris aside from increasing speed, score, and next display.

---
+++ GameBoard.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameBoard : MonoBehaviour {
+
+    public static Transform[,] gameBoard = new Transform[10, 20];
+
+    public static void PrintArray()
+    {
+        string arrayOutput = "";
+
+        int iMax = gameBoard.GetLength(0) - 1;
+        int jMax = gameBoard.GetLength(1) - 1;
+
+        for(int j = jMax; j>= 0; j--)
+        {
+            for (int i = 0; i <= iMax; i++)
+            {
+
+                if (gameBoard[i, j] == null)
+                {
+                    arrayOutput += "N ";
+                }
+                else
+                {
+                    arrayOutput += "X";
+                }
+            }
+            arrayOutput += "\n \n";
+        }
+        var myArrayComp = GameObject.Find("MyArray").GetComponent<Text>();
+        myArrayComp.text = arrayOutput;
+    }
+    public static bool DeleteAllFullRows()
+    {
+        for (int row = 0; row < 20; ++ row)
+        {
+            if(IsRowFull(row))
+            {
+                DeleteGBRow(row);
+                //make sound
+
+                return true;
+            }
+        }
+        return false;
+    }
+    public static bool IsRowFull(int row)
+    {
+        for (int col = 0; col < 10; ++col)
+        {
+            if (gameBoard[col, row] == null)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void DeleteGBRow(int row)
+    {
+        for (int col = 0; col < 10; ++ col)
+        {
+            Destroy(gameBoard[col, row].gameObject);
+            gameBoard[col, row] = null;
+        }
+
+        row++;
+
+        for(int j = row; j < 20; ++j)
+        {
+            for(int col = 0; col < 10; ++col)
+            {
+                if (gameBoard[col,j] != null)
+                {
+                    gameBoard[col, j -1] = gameBoard[col, j];
+
+                    gameBoard[col, j] = null;
+
+                    gameBoard[col, j - 1].position += new Vector3(0, -1, 0);
+                }
+            }
+        }
+    }
+}
--- Shapes.cs
+++ Shapes.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shapes : MonoBehaviour {
 
@@ -19,32 +20,52 @@ public class Shapes : MonoBehaviour {
         {
             transform.position += new Vector3(-1, 0, 0);
 
-            Debug.Log(transform.position);
-
             if(!IsInGrid())
             {
                 transform.position += new Vector3(1, 0, 0);
+            }
+            else
+            {
+                UpdateGameBoard();
             }
         }
         if (Input.GetKeyDown("d"))
         {
             transform.position += new Vector3(1, 0, 0);
 
-            Debug.Log(transform.position);
             if (!IsInGrid())
             {
                 transform.position += new Vector3(-1, 0, 0);
+            }
+            else
+            {
+                UpdateGameBoard();
             }
         }
         if (Input.GetKeyDown("s") || Time.time -lastMoveDown >= 1)
         {
             transform.position += new Vector3(0, -1, 0);
 
-            Debug.Log(transform.position);
-
             if (!IsInGrid())
             {
                 transform.position += new Vector3(0, 1, 0);
+
+                bool rowDeleted = GameBoard.DeleteAllFullRows();
+
+                if(rowDeleted)
+                {
+                    GameBoard.DeleteAllFullRows();
+
+                    IncreaseTextUIScore();
+                }
+
+                enabled = false;
+
+                FindObjectOfType<ShapeSpawner>().SpawnShape();
+            }
+            else
+            {
+                UpdateGameBoard();
             }
 
             lastMoveDown = Time.time;
@@ -53,35 +74,91 @@ public class Shapes : MonoBehaviour {
         {
             transform.Rotate(0, 0, 90);
 
-            Debug.Log(transform.position);
-
             if (!IsInGrid())
             {
                 transform.Rotate(0, 0, -90);
+            }
+            else
+            {
+                UpdateGameBoard();
+            }
+        }
+        if (Input.GetKeyDown("e"))
+        {
+            transform.Rotate(0, 0, -90);
+
+            if (!IsInGrid())
+            {
+                transform.Rotate(0, 0, 90);
+            }
+            else
+            {
+                UpdateGameBoard();
             }
         }
     }
 
     public bool IsInGrid()
-    {
-        int childCount = 0;
-        
+    {      
         foreach(Transform childBlock in transform)
         {
-            Vector2 vect = childBlock.position;
-            childCount++;
-
-            Debug.Log(childCount + " " + childBlock.position);
-
+            Vector2 vect = RoundVector(childBlock.position);
             if(!IsInBorder(vect))
+            {
+                return false;
+            }
+
+            if(GameBoard.gameBoard[(int)vect.x, (int)vect.y] != null && GameBoard.gameBoard[(int)vect.x, (int)vect.y].parent != transform)
             {
                 return false;
             }
         }
         return true;
     }
+
+    public Vector2 RoundVector(Vector2 vect)
+    {
+        return new Vector2(Mathf.Round(vect.x), Mathf.Round(vect.y));
+    }
     public static bool IsInBorder(Vector2 pos)
     {
-            return ((int)pos.x >= -5.63 && (int)pos.x <= 3.371 && (int)pos.y >= -9.49);
+            return ((int)pos.x >= 0 && (int)pos.x <= 9 && (int)pos.y >= 0);
+    }
+
+    public void UpdateGameBoard()
+    {
+        for(int y = 0; y < 20; ++y)
+        {
+            for(int x = 0; x<10; ++x)
+            {
+                if(GameBoard.gameBoard[x,y] != null && GameBoard.gameBoard[x, y].parent == transform)
+                    {
+                        GameBoard.gameBoard[x, y] = null;
+                    }
+            }
+        }
+
+        foreach(Transform childBlock in transform)
+        {
+            Vector2 vect = RoundVector(childBlock.position);
+
+            GameBoard.gameBoard[(int)vect.x, (int)vect.y] = childBlock;
+
+            Debug.Log("Cube At : " + vect.x + " " + vect.y);
+
+        }
+
+        //GameBoard.PrintArray();
+    }
+
+    void IncreaseTextUIScore()
+    {
+        var textUIComp = GameObject.Find("Score").GetComponent<Text>();
+
+        int score = int.Parse(textUIComp.text);
+
+        score++;
+
+        textUIComp.text = score.ToString();
     }
 }
