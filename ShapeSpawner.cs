ff977a73f1738845d4d38939d2fa999a65e9e317
Nicholas Otto <43212390+nso332@users.noreply.github.com>
2018-12-09 02:29:55 -0600

    Add files via upload

---
+++ ShapeSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeSpawner : MonoBehaviour {
+
+    public GameObject[] shapes;
+
+    public void SpawnShape()
+    {
+        int shapeIndex = Random.Range(0, 6);
+
+        Instantiate(shapes[shapeIndex], transform.position, Quaternion.identity);
+        // shape index between 0-6, transform positoin is the position of the shape spawner, quaternion handles rotation
+    }
+
+	// Use this for initialization
+	void Start () {
+        SpawnShape();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Shapes.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shapes : MonoBehaviour {
+
+    public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("a"))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+
+            Debug.Log(transform.position);
+
+            if(!IsInBorder(transform.position))
+            {
+                transform.position += new Vector3(1, 0, 0);
+            }
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            transform.position += new Vector3(1, 0, 0);
+
+            Debug.Log(transform.position);
+        }
+        if (Input.GetKeyDown("s"))
+        {
+            transform.position += new Vector3(0, -1, 0);
+
+            Debug.Log(transform.position);
+        }
+        if (Input.GetKeyDown("w"))
+        {
+            transform.Rotate(0, 0, 90);
+
+            Debug.Log(transform.position);
+        }
+
+        
+    }
+    public static bool IsInBorder(Vector2 pos)
+    {
+            return ((int)pos.x >= -5.6 && (int)pos.x <= 2.4 && (int)pos.y >= -9.8);
+    }
+}
